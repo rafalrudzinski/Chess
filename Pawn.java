ca27ec0bed593ef39f62f8932e9fbcde46d2fbcf
Nick Okerberg <nick-okerberg@users.noreply.github.com>
2016-09-26 17:35:05 -0500

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -1,27 +1,56 @@
+
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+
+
+
+
 
 public class Pawn extends JPanel{
-	BufferedImage black = ImageIO.read(new File("resources/black.png"));
-	JLabel image = new JLabel(new ImageIcon(black));	
-	JPanel pawn = new JPanel();
 	
+	int player = -1;
+	BufferedImage pawnImg;
+	JLabel theImage;
+	JPanel pawn;
+	JButton pawnJbt;
+	ImageIcon pawnIcon;
+
 	
-	
-	public Pawn() throws IOException{
-	pawn.setSize(64,64);	
+	Pawn(int player) throws IOException{
+		this.player = player;
+
+		if (this.player == 1) {
+			pawnIcon = new ImageIcon("images/d1_60x60_Pawn1.png");
+		}
+		else if (this.player == 2) {
+			pawnIcon = new ImageIcon("images/d1_60x60_Pawn2.png");
+		}
 		
+		//JLabel theImage = new JLabel(new ImageIcon(pawnImg));
+		//JPanel pawn = new JPanel();
+		//pawn.setSize(100,100);
+		pawnJbt = new JButton(pawnIcon);
+		pawnJbt.setSize(100, 100);
+		pawnJbt.setOpaque(false);
+		pawnJbt.setContentAreaFilled(false);
+		pawnJbt.setBorderPainted(false);
+
 		
 	}
+	
+
 	public void setPawn() throws IOException{
-		
 	}
-
-
+	
+	public JButton getPawn(){
+		return pawnJbt;
+	}
+	
 }
