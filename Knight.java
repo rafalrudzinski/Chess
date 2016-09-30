783491452c9e7ebf5ec6bcf776ff42e47f36d284
Justin Bee <jbee2@asu.edu>
2016-09-30 09:40:35 -0400

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,15 +1,37 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class Knight extends Piece {
-	int color;
-	static ImageIcon white; 
-	static ImageIcon black;
+
+public class Knight extends JPanel{
+	int player = -1;
+	BufferedImage KnightImg;
+    JPanel knight;
+	JLabel knightJbt;
+	ImageIcon knightIcon;
+	
+
+	Knight(int player) throws IOException{
+		this.player = player;
+		if (this.player == 1) {
+			knightIcon = new ImageIcon("images/d1_60x60_Knight1.png");
+		}
+		else if (this.player == 2) {
+			knightIcon = new ImageIcon("images/d1_60x60_Knight2.png");
+		}
 		
-	public Knight() {
-		super(white = new ImageIcon("src/images/d1_60x60_Knight2.png"));
+		knightJbt = new JLabel(knightIcon);
+		knightJbt.setSize(100, 100);
+		knightJbt.setOpaque(false);
+
 	}
-		
-	public Knight(int color) {
-		super(black = new ImageIcon("src/images/d1_60x60_Knight1.png"));
+
+	public void setKnight() throws IOException{
+	}
+
+	public JLabel getKnight(){
+		return knightJbt;
 	}
 }
