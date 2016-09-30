b009e26dd2fd6798b8293b8bc4a622fb5b728a25
Justin Bee <jbee2@asu.edu>
2016-09-30 09:39:44 -0400

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -1,16 +1,37 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class Queen extends Piece {
-		
-	int color;
-	static ImageIcon white; 
-	static ImageIcon black;
-		
-	public Queen() {
-		super(white = new ImageIcon("src/images/d1_60x60_Queen2.png"));
+
+
+public class Queen extends JPanel{
+	int player = -1;
+	BufferedImage queenImg;
+	JPanel queen;
+	JLabel queenJbt;
+	ImageIcon queenIcon;
+
+	Queen(int player) throws IOException{
+		this.player = player;
+		if (this.player == 1) {
+			queenIcon = new ImageIcon("images/d1_60x60_Queen1.png");
+		}
+		else if (this.player == 2) {
+			queenIcon = new ImageIcon("images/d1_60x60_Queen2.png");
+		}
+		queenJbt = new JLabel(queenIcon);
+		queenJbt.setSize(100, 100);
+		queenJbt.setOpaque(false);
 	}
-		
-	public Queen(int color) {
-		super(black = new ImageIcon("src/images/d1_60x60_Queen1.png"));
+
+
+	public void setQueen() throws IOException{
+	}
+
+	public JLabel getQueen(){
+		return queenJbt;
 	}
 }
+
