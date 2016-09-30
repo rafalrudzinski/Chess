117142588316411fab7df592c5beb25f25fa62eb
Justin Bee <jbee2@asu.edu>
2016-09-30 09:38:47 -0400

    Update King.java

--- King.java
+++ King.java
@@ -1,15 +1,38 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class King extends Piece {
-	int color;
-	static ImageIcon white; 
-	static ImageIcon black;
-		
-	public King() {
-		super(white = new ImageIcon("src/images/d1_60x60_King1.png"));
+public class King extends JPanel{
+	int player = -1;
+	BufferedImage kingImg;
+	JPanel king;
+	JLabel kingJbt;
+	ImageIcon kingIcon;
+	
+
+	King(int player) throws IOException{
+		this.player = player;
+		if (this.player == 1) {
+			kingIcon = new ImageIcon("images/d1_60x60_King1.png");
+		}
+		else if (this.player == 2) {
+			kingIcon = new ImageIcon("images/d1_60x60_King2.png");
+		}
+
+		kingJbt = new JLabel(kingIcon);
+		kingJbt.setSize(100, 100);
+		kingJbt.setOpaque(false);
+
 	}
-		
-	public King(int color) {
-		super(black = new ImageIcon("src/images/d1_60x60_King2.png"));
+
+	public void setKing() throws IOException{
 	}
+
+	public JLabel getKing(){
+		return kingJbt;
+	}
+	
+
 }
