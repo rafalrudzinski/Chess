3098a0176476ada6bd1db7951a55114ebb3983b6
Justin Bee <jbee2@asu.edu>
2016-09-30 09:41:30 -0400

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -1,15 +1,38 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class Rook extends Piece {
-	int color;
-	static ImageIcon white; 
-	static ImageIcon black;
-		
-	public Rook() {
-		super(white = new ImageIcon("src/images/d1_60x60_Rook2.png"));
+
+public class Rook extends JPanel{
+
+	int player = -1;
+	BufferedImage RookImg;
+	JPanel rook;
+	JLabel rookJbt;
+
+	ImageIcon rookIcon;
+
+	Rook(int player) throws IOException{
+		this.player = player;
+		if (this.player == 1) {
+			rookIcon = new ImageIcon("images/d1_60x60_Rook1.png");
+		}
+		else if (this.player == 2) {
+			rookIcon = new ImageIcon("images/d1_60x60_Rook2.png");
+		}
+
+		rookJbt = new JLabel(rookIcon);
+		rookJbt.setSize(100, 100);
+		rookJbt.setOpaque(false);
+
 	}
-		
-	public Rook(int color) {
-		super(black = new ImageIcon("src/images/d1_60x60_Rook1.png"));
+
+	public void setRook() throws IOException{
+	}
+
+	public JLabel getRook(){
+		return rookJbt;
 	}
 }
